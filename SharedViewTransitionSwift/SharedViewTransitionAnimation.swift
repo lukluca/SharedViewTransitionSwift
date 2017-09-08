5a3d1742b9a01939027abfc83746428b56676dce
Nicolò Pasini <nicolo.pasini@accenture.com>
2017-09-08 11:36:10 +0200

    Added class for animation

---
+++ SharedViewTransitionSwift/SharedViewTransitionAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  SharedViewTransitionAnimation.swift
+//  SharedViewTransitionSwift
+//
+//  Created by Pasini, Nicolò on 08/09/2017.
+//  Copyright © 2017 Tagliabue, L. All rights reserved.
+//
+
+import UIKit
+
+class SharedViewTransitionAnimation: NSObject {
+
+}
